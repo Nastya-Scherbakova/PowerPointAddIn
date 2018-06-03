49af0aa383a46098064a15e52f006c06d2420a7f
Nastya Scherbakova <danskar@yandex.ua>
2018-06-03 21:58:46 +0300

    init commit

---
+++ PowerPointAddIn/Ribbon.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace PowerPointAddIn
+{
+    partial class Ribbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.changer = this.Factory.CreateRibbonButton();
+            this.editBox1 = this.Factory.CreateRibbonEditBox();
+            this.tab1.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.editBox1);
+            this.group1.Items.Add(this.changer);
+            this.group1.Label = "Button group";
+            this.group1.Name = "group1";
+            // 
+            // changer
+            // 
+            this.changer.Label = "Change text";
+            this.changer.Name = "changer";
+            this.changer.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.changer_Click);
+            // 
+            // editBox1
+            // 
+            this.editBox1.Label = "Points";
+            this.editBox1.MaxLength = 5;
+            this.editBox1.Name = "editBox1";
+            this.editBox1.Text = "10000";
+            // 
+            // Ribbon
+            // 
+            this.Name = "Ribbon";
+            this.RibbonType = "Microsoft.PowerPoint.Presentation";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton changer;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox editBox1;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon Ribbon
+        {
+            get { return this.GetRibbon<Ribbon>(); }
+        }
+    }
+}
---
+++ PowerPointAddIn/Ribbon.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Interop.PowerPoint;
+using Microsoft.Office.Tools.Ribbon;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+
+namespace PowerPointAddIn
+{
+    public partial class Ribbon
+    {
+        private void Ribbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void changer_Click(object sender, RibbonControlEventArgs e)
+        {
+            work_with_Slides(Globals.ThisAddIn.Application.ActiveWindow.Selection.SlideRange);
+
+
+        }
+        private void work_with_Slides(SlideRange SldRange)
+        {
+            PowerPoint.Presentation currentPresentation = Globals.ThisAddIn.Application.ActivePresentation;
+            float slideWidth = currentPresentation.PageSetup.SlideWidth;
+
+            foreach (Slide slide in SldRange)
+            {
+                foreach (Shape shape in slide.Shapes)
+                {
+
+                    if (shape.Type == Microsoft.Office.Core.MsoShapeType.msoTextBox)
+                    {
+                       
+                        
+                        if((shape.Left + float.Parse(editBox1.Text, CultureInfo.InvariantCulture) * 1.333f) < slideWidth)
+                        {
+                             shape.Left += float.Parse(editBox1.Text, CultureInfo.InvariantCulture) * 1.333f;
+                        }
+                       
+
+                        shape.TextEffect.FontBold = Microsoft.Office.Core.MsoTriState.msoCTrue;
+                        shape.TextFrame.TextRange.Font.Color.RGB = System.Drawing.ColorTranslator.ToOle(Color.Red);
+                    }
+                }
+            }
+        }
+    }
+}
